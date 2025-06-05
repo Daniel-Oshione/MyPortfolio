6e3b1c7fea1e6bbc6fa3f43549ce722d15d0abb3
Daniel-Oshione <omobsdaniel@gmail.com>
2025-06-05 20:09:49 +0100

    fixed the navbar

--- home.js
+++ home.js
@@ -1,8 +1,17 @@
 document.addEventListener('DOMContentLoaded', function() {
     const toggleButton = document.getElementById('toggle');
     const dropDown = document.querySelector('.drop_down');
+    const head = document.getElementById('head');
   
-    toggleButton.addEventListener('click', function() {
+    toggleButton.addEventListener("click", () => {
       dropDown.classList.toggle('visible');
+      
+      if (dropDown.classList.toggle('visble')) {
+        head.style.backdropFilter= "blur(4px)";
+      }
+      else{
+        head.style.backdropFilter= "blur(0)";
+      }
     });
   });
+
