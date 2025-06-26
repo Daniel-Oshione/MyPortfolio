db550bb94658f512ac9254bd821f1068c2677419
Daniel-Oshione <omobsdaniel@gmail.com>
2025-06-26 15:28:40 +0100

    fixed link errors and alignments

--- home.js
+++ home.js
@@ -2,15 +2,18 @@ document.addEventListener('DOMContentLoaded', function() {
     const toggleButton = document.getElementById('toggle');
     const dropDown = document.querySelector('.drop_down');
     const head = document.getElementById('head');
+    const body = document.getElementById('body')
   
     toggleButton.addEventListener("click", () => {
       dropDown.classList.toggle('visible');
       
       if (dropDown.classList.toggle('visble')) {
         head.style.backdropFilter= "blur(4px)";
+        body.style.overflow = "hidden";
       }
       else{
         head.style.backdropFilter= "blur(0)";
+        body.style.overflow = "visible";
       }
     });
   });
